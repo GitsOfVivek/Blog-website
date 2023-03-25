3333e397b66ee948857456f3e9d003f1150fe86d
GitsOfVivek <singhvivek1999@yahoo.com>
2023-03-25 11:46:55 +0530

    firestore added

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyAu1o4V8CnXvk7_nGTYv_QYc4SPP6sqjVE',
+	authDomain: 'blog-website-6fc6c.firebaseapp.com',
+	projectId: 'blog-website-6fc6c',
+	storageBucket: 'blog-website-6fc6c.appspot.com',
+	messagingSenderId: '574036036015',
+	appId: '1:574036036015:web:57a04376eec5034cdfc75a',
+	measurementId: 'G-45KK5ZN8MM',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
