ab6bae1438cf5b62c2fd29248a8660d45740615e
GitsOfVivek <singhvivek1999@yahoo.com>
2023-03-28 23:16:22 +0530

    loading animation added

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import loading from '../../assets/loading-2.svg';
 
 const Loading = () => {
 	return (
 		<LoadingWrapper>
-			<div>Loading...</div>
+			<div>
+				<img height="200" width="200" src={loading} alt="loading..." />
+			</div>
 		</LoadingWrapper>
 	);
 };
